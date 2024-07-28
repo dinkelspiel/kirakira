801f2fc9d0dcc88e3a6916d6a7619440c913062f
dinkelspiel <mail@keii.dev>
2024-07-29 01:31:26 +0200

    fix: error in db.sql

--- backend/db.sql
+++ backend/db.sql
@@ -235,7 +235,7 @@ CREATE TABLE `tag` (
   ) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 /*!40101 SET character_set_client = @saved_cs_client */
