d2e608a456153218a4b94401646fd8f6cb302fa4
dinkelspiel <mail@keii.dev>
2024-08-03 14:54:23 +0200

    build: move api url to env var in prod

--- run-prod.sh
+++ run-prod.sh
@@ -2,7 +2,7 @@
 sh ./run-clean.sh
 # run
 cd ./client
-echo "pub fn get_api_url() { \"http://localhost:8001\" }" > ./src/env.gleam
+echo "pub fn get_api_url() { \"${API_URL}\" }" > ./src/env.gleam
 gleam run -m lustre/dev build --outdir=../server/priv/static --minify
 cd ../server
 gleam run
