87467ce0340fe77b469c97ce44d383b74530bbdf
dinkelspiel <mail@keii.dev>
2024-08-03 14:49:33 +0200

    build: remove env vars from run prod sh file

--- run-prod.sh
+++ run-prod.sh
@@ -5,4 +5,4 @@ cd ./client
 echo "pub fn get_api_url() { \"http://localhost:8001\" }" > ./src/env.gleam
 gleam run -m lustre/dev build --outdir=../server/priv/static --minify
 cd ../server
-DB_HOST=mysql DB_PASSWORD=kirakira DB_USER=root DB_NAME=kirakira DB_PORT=3306 gleam run
+gleam run
