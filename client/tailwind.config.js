bbd0745395c52699e4c9fda6224844b05265b1a1
dinkelspiel <mail@keii.dev>
2025-06-26 17:26:16 +0900

    fix: css not being generated by lustre dev tools

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{gleam,mjs}",
-    "../hermodr/src/**/*.{gleam,mjs}",
-  ],
-  theme: {
-    extend: {
-      animation: {
-        wiggle: "wiggle 0.3s linear infinite",
-      },
-      keyframes: {
-        wiggle: {
-          "0%, 100%": { transform: "rotate(-8deg)" },
-          "50%": { transform: "rotate(8deg)" },
+    content: [
+        "./priv/static/index.html",
+        "./src/**/*.{gleam,mjs}",
+        "../hermodr/src/**/*.{gleam,mjs}",
+    ],
+    theme: {
+        extend: {
+            animation: {
+                wiggle: "wiggle 0.3s linear infinite",
+            },
+            keyframes: {
+                wiggle: {
+                    "0%, 100%": { transform: "rotate(-8deg)" },
+                    "50%": { transform: "rotate(8deg)" },
+                },
+            },
         },
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
